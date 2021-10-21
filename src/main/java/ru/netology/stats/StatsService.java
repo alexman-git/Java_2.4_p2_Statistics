f80605807b769638f0b143941d2eb2fe85399555
Alexander Maletin <man.alex212@gmail.com>
2021-10-21 18:59:50 +0500

    First commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,73 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long totalSales(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public long averageMonthSale(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum / sales.length;
+    }
+
+    public int maxSalesMonth(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSalesMonth(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public long monthsLowerAverage(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        long average = sum / sales.length;
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale < average) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public long monthsHigherAverage(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        long average = sum / sales.length;
+        int counter = 0;
+        for (long sale : sales) {
+            if (sale > average) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,62 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldCountTotalSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+        long actual = service.totalSales(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCountAverageMonthSale() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+        long actual = service.averageMonthSale(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindMaxSalesMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+        long actual = service.maxSalesMonth(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindMinSalesMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+        long actual = service.minSalesMonth(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCountMonthsLowerAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.monthsLowerAverage(sales);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCountMonthsHigherAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.monthsHigherAverage(sales);
+        assertEquals(expected, actual);
+    }
+}
